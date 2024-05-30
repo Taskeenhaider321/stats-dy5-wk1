7a0ded6506830fa649a5aa920f0e6545a86d1afb
Taskeenhaider509 <taskeen_haider_intern@netixsol.com>
2024-05-30 21:19:22 +0500

    Version-1

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        containerColor: '#1B1938',
+        insightColor: '#A95BDA'
+      },
+    },
+  },
+  plugins: [],
+}
+
